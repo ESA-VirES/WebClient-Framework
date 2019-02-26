5eabdc69053d9bc2e5f33c8543a58c837cce3577
Martin Paces <martin.paces@eox.at>
2019-02-26 11:20:17 +0100

    Removing duplicate variables.

--- app/scripts/controller/DataController.js
+++ app/scripts/controller/DataController.js
@@ -278,7 +278,6 @@
             "IRC", "IRC_Error", "FAC", "FAC_Error",
             "EEF", "RelErr", "OrbitNumber", "OrbitDirection", "QDOrbitDirection",
             "SunDeclination","SunRightAscension","SunHourAngle","SunAzimuthAngle","SunZenithAngle",
-            "OrbitDirection","QDOrbitDirection",
             // New models
             "F_res_MCO_SHA_2C", "B_NEC_res_MCO_SHA_2C",
             "F_res_MCO_SHA_2D", "B_NEC_res_MCO_SHA_2D",
