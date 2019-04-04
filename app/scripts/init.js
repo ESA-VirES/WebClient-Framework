7988145d2272dfb1869738047bf03ce2f957a66f
Martin Paces <martin.paces@eox.at>
2019-04-04 12:29:16 +0200

    Forcing scripts reload.

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -4,7 +4,7 @@
     var root = this;
 
     root.require.config({
-        urlArgs: 'bust=v2.22',
+        urlArgs: 'bust=v2.23',
 
         waitSeconds: 120,
         /* starting point for application */
