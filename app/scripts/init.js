c7ac95518ad3a0bbb92180c135d252fa1dc1955a
Martin Paces <martin.paces@eox.at>
2023-08-24 21:18:29 +0200

    Version bump (v3.12.0).

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -4,7 +4,7 @@
     var root = this;
 
     root.require.config({
-        urlArgs: 'bust=v3.11.0',
+        urlArgs: 'bust=v3.12.0',
 
         waitSeconds: 120,
         /* starting point for application */
